bbd061fac347c4f694061ffb6bb35ccf8ef9ae8f
Josh Tilles <MerelyAPseudonym@gmail.com>
2018-02-13 11:12:58 -0500

    minimal update (still need shasum)

--- mysql@5.6/plan.sh
+++ mysql@5.6/plan.sh
@@ -1,9 +1,10 @@
 pkg_name=mysql
 pkg_origin=core
-pkg_version=5.7.17
+pkg_version=5.6.39
 pkg_maintainer='The Habitat Maintainers <humans@habitat.sh>'
 pkg_license=('GPL-2.0')
-pkg_source=http://dev.mysql.com/get/Downloads/MySQL-5.7/${pkg_name}-${pkg_version}.tar.gz
+pkg_source=http://dev.mysql.com/get/Downloads/MySQL-5.6/${pkg_name}-${pkg_version}.tar.gz
+# https://dev.mysql.com/downloads/file/?id=474438
 pkg_shasum=cebf23e858aee11e354c57d30de7a079754bdc2ef85eb684782458332a4b9651
 pkg_upstream_url=https://www.mysql.com/
 pkg_description=$(cat << EOF
@@ -18,27 +19,28 @@ Set the app_username and app_password at runtime to have that user created, it w
 EOF
 )
 pkg_deps=(
-  core/coreutils
-  core/gawk
-  core/gcc-libs
-  core/glibc
-  core/grep
-  core/inetutils
-  core/ncurses
-  core/openssl
-  core/pcre
-  core/perl
-  core/procps-ng
-  core/sed
+  # core/coreutils
+  # core/gawk
+  # core/gcc-libs
+  # core/glibc
+  # core/grep
+  # core/inetutils
+  # core/ncurses
+  # core/openssl
+  # core/pcre
+  # core/perl
+  # core/procps-ng
+  # core/sed
 )
 pkg_build_deps=(
-  core/bison
-  core/boost159
+  # core/bison
+  # core/boost159
   core/cmake
-  core/diffutils
-  core/gcc
-  core/make
-  core/patch
+  # core/diffutils
+  # core/gcc
+  # core/make
+  # core/patch
+  core/ninja
 )
 pkg_svc_user="hab"
 pkg_bin_dirs=(bin)
@@ -52,19 +54,21 @@ pkg_exports=(
 )
 
 do_build() {
-  cmake . -DLOCAL_BOOST_DIR="$(pkg_path_for core/boost159)" \
-          -DBOOST_INCLUDE_DIR="$(pkg_path_for core/boost159)/include" \
-          -DWITH_BOOST="$(pkg_path_for core/boost159)" \
-          -DCURSES_INCLUDE_PATH="$(pkg_path_for core/ncurses)/include" \
-          -DCURSES_LIBRARY="$(pkg_path_for core/ncurses)/lib/libcurses.so" \
-          -DWITH_SSL=yes \
-          -DOPENSSL_INCLUDE_DIR="$(pkg_path_for core/openssl)/include" \
-          -DOPENSSL_LIBRARY="$(pkg_path_for core/openssl)/lib/libssl.so" \
-          -DCRYPTO_LIBRARY="$(pkg_path_for core/openssl)/lib/libcrypto.so" \
+  cmake . \
+          ## -DLOCAL_BOOST_DIR="$(pkg_path_for core/boost159)" \
+          ## -DBOOST_INCLUDE_DIR="$(pkg_path_for core/boost159)/include" \
+          # -DWITH_BOOST="$(pkg_path_for core/boost159)" \
+          # -DCURSES_INCLUDE_PATH="$(pkg_path_for core/ncurses)/include" \
+          # -DCURSES_LIBRARY="$(pkg_path_for core/ncurses)/lib/libcurses.so" \
+#          -DWITH_SSL=yes \
+          # -DOPENSSL_INCLUDE_DIR="$(pkg_path_for core/openssl)/include" \
+          # -DOPENSSL_LIBRARY="$(pkg_path_for core/openssl)/lib/libssl.so" \
+          # -DCRYPTO_LIBRARY="$(pkg_path_for core/openssl)/lib/libcrypto.so" \
           -DCMAKE_INSTALL_PREFIX="$pkg_prefix" \
           -DWITH_EMBEDDED_SERVER=no \
           -DWITH_EMBEDDED_SHARED_LIBRARY=no
-  make
+  # make
+  ninja
 }
 
 do_install() {
